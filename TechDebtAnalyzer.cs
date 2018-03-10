60f0773f7e6cef0dbc7235eb291ca973d16cbf61
David Higgins <david@reincubate.com>
2018-03-10 12:19:02 +0000

    hmm

--- TechDebtAnalyzer.cs
+++ TechDebtAnalyzer.cs
@@ -21,7 +21,8 @@ namespace TechDebt
         {
             get
             {
-                return ImmutableArray.Create(Rules.Value.Values.Concat(new[] { riskRule, issueRule, diagRule }).ToArray());
+                //return ImmutableArray.Create(Rules.Value.Values.Concat(new[] { riskRule, issueRule, diagRule }).ToArray());
+                return ImmutableArray.Create(diagRule);
             }
         }
 
@@ -35,8 +36,8 @@ namespace TechDebt
                                 )
         );
 
-        static DiagnosticDescriptor riskRule = new DiagnosticDescriptor("Risk", "Risk", "{0}", "Risk", DiagnosticSeverity.Info, true, "");
-        static DiagnosticDescriptor issueRule = new DiagnosticDescriptor("Issue", "Issue", "{0}", "Issue", DiagnosticSeverity.Info, true, "");
+        //static DiagnosticDescriptor riskRule = new DiagnosticDescriptor("Risk", "Risk", "{0}", "Risk", DiagnosticSeverity.Info, true, "");
+        //static DiagnosticDescriptor issueRule = new DiagnosticDescriptor("Issue", "Issue", "{0}", "Issue", DiagnosticSeverity.Info, true, "");
         static DiagnosticDescriptor diagRule = new DiagnosticDescriptor("Diag", "Diag", "{0}", "Diag", DiagnosticSeverity.Info, true, "");
 
         public override void Initialize(AnalysisContext context)
@@ -57,43 +58,43 @@ namespace TechDebt
                 context.ReportDiagnostic(Diagnostic.Create(diagRule, loc, fa.AttributeClass.ToDisplayString()));
             }
 
-            var df = tda.FirstOrDefault(x => context.Compilation.GetTypeByMetadataName(typeof(SmellAttribute).FullName) == x.AttributeClass);
-            if (df != null)
-            {
-                var dca = df.ConstructorArguments;
-                var code = (Smell)dca[0].Value;
-                var rule = Rules.Value[code];
-                var props = new Dictionary<String, String>
-                {
-                    { "gap", dca[2].Value.ToString() }
-                };
-                var diagnostic = Diagnostic.Create(rule, loc, props.ToImmutableDictionary(), dca[1].Value as String);
-                context.ReportDiagnostic(diagnostic);
-            }
+            //var df = tda.FirstOrDefault(x => context.Compilation.GetTypeByMetadataName(typeof(SmellAttribute).FullName) == x.AttributeClass);
+            //if (df != null)
+            //{
+            //    var dca = df.ConstructorArguments;
+            //    var code = (Smell)dca[0].Value;
+            //    var rule = Rules.Value[code];
+            //    var props = new Dictionary<String, String>
+            //    {
+            //        { "gap", dca[2].Value.ToString() }
+            //    };
+            //    var diagnostic = Diagnostic.Create(rule, loc, props.ToImmutableDictionary(), dca[1].Value as String);
+            //    context.ReportDiagnostic(diagnostic);
+            //}
 
-            var df2 = tda.FirstOrDefault(x =>  context.Compilation.GetTypeByMetadataName(typeof(RiskAttribute).FullName) == x.AttributeClass);
-            if (df2 != null)
-            {
-                var dca = df2.ConstructorArguments;
-                var props = new Dictionary<String, String>
-                {
-                    { "gap", dca[1].Value.ToString() }
-                };
-                var diagnostic = Diagnostic.Create(riskRule, loc, props.ToImmutableDictionary(), dca[0].Value as String);
-                context.ReportDiagnostic(diagnostic);
-            }
+            //var df2 = tda.FirstOrDefault(x =>  context.Compilation.GetTypeByMetadataName(typeof(RiskAttribute).FullName) == x.AttributeClass);
+            //if (df2 != null)
+            //{
+            //    var dca = df2.ConstructorArguments;
+            //    var props = new Dictionary<String, String>
+            //    {
+            //        { "gap", dca[1].Value.ToString() }
+            //    };
+            //    var diagnostic = Diagnostic.Create(riskRule, loc, props.ToImmutableDictionary(), dca[0].Value as String);
+            //    context.ReportDiagnostic(diagnostic);
+            //}
 
-            var df3 = tda.FirstOrDefault(x => context.Compilation.GetTypeByMetadataName(typeof(IssueAttribute).FullName) == x.AttributeClass);
-            if (df3 != null)
-            {
-                var dca = df3.ConstructorArguments;
-                var props = new Dictionary<String, String>
-                {
-                    { "gap", dca[1].Value.ToString() }
-                };
-                var diagnostic = Diagnostic.Create(issueRule, loc, props.ToImmutableDictionary(), dca[0].Value as String);
-                context.ReportDiagnostic(diagnostic);
-            }
+            //var df3 = tda.FirstOrDefault(x => context.Compilation.GetTypeByMetadataName(typeof(IssueAttribute).FullName) == x.AttributeClass);
+            //if (df3 != null)
+            //{
+            //    var dca = df3.ConstructorArguments;
+            //    var props = new Dictionary<String, String>
+            //    {
+            //        { "gap", dca[1].Value.ToString() }
+            //    };
+            //    var diagnostic = Diagnostic.Create(issueRule, loc, props.ToImmutableDictionary(), dca[0].Value as String);
+            //    context.ReportDiagnostic(diagnostic);
+            //}
         }
     }
 }
